2403455cd7b0c02b77be518b4834ee84be1ba19b
JavierJia <jianfeng.jia@gmail.com>
2014-03-30 01:45:22 -0700

    test pi

---
+++ hama/src/main/java/hama/TestPi.java
@@ -0,0 +1,12 @@
+package hama;
+
+import java.io.IOException;
+
+import org.apache.hama.examples.PiEstimator;
+
+public class TestPi {
+	public static void main(String[] args) throws IOException,
+			InterruptedException, ClassNotFoundException {
+		PiEstimator.main(args);
+	}
+}
